5af33d085c25fc76d22a74c56dd587177211272a
IslanP1 <islanpereiradeoliveira@gmail.com>
2023-09-28 20:45:21 -0300

    feat: questão 04 adicionada

--- atividade01/q04/script.js
+++ atividade01/q04/script.js
@@ -1,2 +1,29 @@
-const tempCelsius = document.getElementById('tempCelsius').value;
-const botao = document.getElementById('calcular');
+const tempCelsius = document.getElementById('celsius');
+const rendFahrenheit = document.getElementById('temp-fahrenheit');
+const rendKelvin = document.getElementById('temp-kelvin');
+
+function converterFahrenheit() {
+    if (tempCelsius.value == ""){
+        rendFahrenheit.innerHTML = "-";
+    } else {
+        const tempFahrenheit = (tempCelsius.value * 9/5) + 32;
+        rendFahrenheit.innerHTML = tempFahrenheit;
+    }
+}
+
+function converterKelvin() {
+    if (tempCelsius.value == ""){
+        rendKelvin.innerHTML = "-";
+    } else {
+        const tempKelvin = parseFloat(tempCelsius.value) + 273.15;
+        rendKelvin.innerHTML = tempKelvin;
+    }
+}
+
+tempCelsius.addEventListener('keyup', () => {
+    converterFahrenheit();
+    converterKelvin();
+});
+
+converterFahrenheit();
+converterKelvin();
