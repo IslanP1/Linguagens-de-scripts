ef5f2aa4b91657a705f184ee8384e3b7e18c1ceb
IslanP1 <islanpereiradeoliveira@gmail.com>
2023-11-04 00:27:53 -0300

    refactor: atividade04 funcao listar

--- atividade04/scripts/index.js
+++ atividade04/scripts/index.js
@@ -5,6 +5,46 @@ let nomeItem = document.querySelector("#nome_item");
 let precoItem = document.querySelector("#preco");
 let statusComprado = document.querySelector("#status_comprado");
 let botaoAdicionar = document.querySelector("#botao_adicionar");
+let listaProdutos = document.querySelector("#lista_produtos");
+
+function renderizarElementos() {
+  let arrayObjetos = listar();
+
+  arrayObjetos.forEach((element, index) => {
+    let elementoTr = document.createElement("tr");
+    let elementoCodigoBarras = document.createElement("td");
+    let elementoNomeItem = document.createElement("td");
+    let elementoPreco = document.createElement("td");
+    let elementoComprado = document.createElement("td");
+    let elementoAcoes = document.createElement("td");
+
+    elementoCodigoBarras.textContent = element.codigo_barras;
+    elementoNomeItem.textContent = element.nome_item;
+    elementoPreco.textContent = element.preco_item;
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = element.status;
+
+    elementoComprado.appendChild(checkbox);
+
+    elementoTr.appendChild(elementoCodigoBarras);
+    elementoTr.appendChild(elementoNomeItem);
+    elementoTr.appendChild(elementoPreco);
+    elementoTr.appendChild(elementoComprado);
+
+    let botao = document.createElement("button");
+    botao.textContent = "Remover";
+    botao.className = "btn btn-primary";
+    botao.id = "botaoRemover";
+
+    elementoAcoes.appendChild(botao);
+
+    elementoTr.appendChild(elementoAcoes);
+
+    listaProdutos.appendChild(elementoTr);
+  });
+}
 
 botaoAdicionar.addEventListener("click", () => {
   adicionar({
@@ -13,6 +53,7 @@ botaoAdicionar.addEventListener("click", () => {
     preco_item: precoItem.value,
     status: statusComprado.checked,
   });
+  renderizarElementos();
 });
 
-listar();
+renderizarElementos();
--- atividade04/scripts/lista.js
+++ atividade04/scripts/lista.js
@@ -3,7 +3,6 @@
 // Preço (Number)
 // Comprado (Boolean)
 const arrayObjetos = JSON.parse(localStorage.getItem("arrayObjetos")) || [];
-let listaProdutos = document.querySelector("#lista_produtos");
 
 function adicionar(item) {
   if (
@@ -13,50 +12,20 @@ function adicionar(item) {
   ) {
     arrayObjetos.push(item);
     localStorage.setItem("arrayObjetos", JSON.stringify(arrayObjetos));
-    listar(); 
   }
 }
 
-function remover(item) {}
+function remover(item) {
+  arrayObjetos.splice(item, 1);
+  localStorage.setItem("arrayObjetos", JSON.stringify(arrayObjetos));
+}
 
 function marcar(item) {}
 
 function desmarcar(item) {}
 
 function listar() {
-  arrayObjetos.forEach(element => {
-    let elementoTr = document.createElement("tr");
-    let elementoCodigoBarras = document.createElement("td");
-    let elementoNomeItem = document.createElement("td");
-    let elementoPreco = document.createElement("td");
-    let elementoComprado = document.createElement("td");
-    let elementoAcoes = document.createElement("td");
-
-    elementoCodigoBarras.textContent = element.codigo_barras;
-    elementoNomeItem.textContent = element.nome_item;
-    elementoPreco.textContent = element.preco_item;
-
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.checked = element.status;
-
-    elementoComprado.appendChild(checkbox);
-    
-    elementoTr.appendChild(elementoCodigoBarras);
-    elementoTr.appendChild(elementoNomeItem);
-    elementoTr.appendChild(elementoPreco);
-    elementoTr.appendChild(elementoComprado);
-
-    let botao = document.createElement("button");
-    botao.textContent = "Remover"
-    botao.className = "btn btn-primary";
-
-    elementoAcoes.appendChild(botao);
-    
-    elementoTr.appendChild(elementoAcoes);
-
-    listaProdutos.appendChild(elementoTr);
-  })
+  return arrayObjetos;
 }
 
 export { adicionar, remover, marcar, desmarcar, listar };
